f7dc864a51e53798feed57e504cc41d7c4f0cdf8
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-05-31 15:21:35 +0200

    tasks

--- imports/ui/templates/tasks/tasks.js
+++ imports/ui/templates/tasks/tasks.js
@@ -4,6 +4,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Projects } from '../../../api/projects/projects.js';
 import { Tasks } from '../../../api/tasks/tasks.js';
 
+import './task_item.html';
 import './tasks.html';
 
 Template.Tasks.helpers({
@@ -17,7 +18,9 @@ Template.Tasks.helpers({
   },
 
   tasks() {
-    return Tasks.find({});
+    const projectID = FlowRouter.getParam('id');
+
+    return Tasks.find({projectID});
   },
 });
 
@@ -25,9 +28,13 @@ Template.Tasks.events({
   "submit form": function(event) {
     event.preventDefault();
 
+    const projectID = FlowRouter.getParam('id');
     const task = event.target.newTask;
 
-    Tasks.insert({ title: task.value });
+    Tasks.insert({
+      title: task.value,
+      projectID
+    });
 
     task.value = '';
   }

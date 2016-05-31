5655df279e77146a00882a251490501789d63bca
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-05-31 15:36:29 +0200

    active route

--- imports/ui/templates/projects/project_item.js
+++ imports/ui/templates/projects/project_item.js
@@ -3,6 +3,12 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import './project_item.html';
 
+Template.ProjectItem.helpers({
+  active() {
+    return this._id===FlowRouter.getParam('id') ? 'active' : '';
+  }
+});
+
 Template.ProjectItem.events({
   "click": function(event) {
     event.preventDefault();
